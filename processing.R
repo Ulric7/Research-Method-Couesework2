1497ac47d8defc16bab7a917e452135f21ad05e3
Ulric7 <116552112+Ulric7@users.noreply.github.com>
2025-04-23 00:15:51 +0100

    Update processing.R

--- processing.R
+++ processing.R
@@ -30,7 +30,7 @@ write.csv(data_cleaned, "data_cleaned.csv", row.names = FALSE)
 
 library(fmsb)
 
-# 选择你要比较的4个群体
+# Select the 4 groups you want to compare
 radar_data <- vegan_summary %>%
   filter((sex == "female" & age_group %in% c("20-29", "60-69")) |
            (sex == "male" & age_group %in% c("20-29", "60-69"))) %>%
@@ -38,40 +38,40 @@ radar_data <- vegan_summary %>%
 
 
 
-# 添加第一行：最大值
+# Add the first line: maximum value
 radar_max <- rep(0.42, ncol(radar_data))
 
-# 添加第二行：最小值
+# Add second line: minimum value
 radar_min <- rep(0.2, ncol(radar_data))
 
-# 合并数据
+# Consolidation of data
 radar_plot_data <- rbind(radar_max, radar_min, radar_data)
 
-# 设置行名（可选）
+# Setting the line name (optional)
 rownames(radar_plot_data) <- c("Max", "Min", 
                                "F-20-29", "F-60-69", "M-20-29", "M-60-69")
 
-# 设置颜色
+# Setting colours
 colors_border <- c("red", "pink", "blue", "skyblue")
 colors_in <- adjustcolor(colors_border, alpha.f = 0.2)
 colnames(radar_plot_data) <- c("GHG", "Land", "WaterScar", "Eutro", "Biodiv", "WaterUse", "Acid")
 
 
-# 雷达图主函数
+# Radargram main function
 radarchart(radar_plot_data,
            axistype = 1,
-           pcol = colors_border,         # 线颜色
-           pfcol = colors_in,            # 填充颜色
-           plwd = 2,                     # 线宽
-           plty = 1,                     # 线型
-           cglcol = "grey",              # 网格线颜色
+           pcol = colors_border,         # thread colour
+           pfcol = colors_in,            # fill colour
+           plwd = 2,                     # line width
+           plty = 1,                     # linear
+           cglcol = "grey",              # Grid line colour
            cglty = 1,
            axislabcol = "black",
            caxislabels = seq(0.2, 0.45, 0.05),
-           vlcex = 1.2                   # 变量标签字体大小
+           vlcex = 1.2                   # Variable Label Font Size
 )
 
-# 添加图例
+# Add Legend
 legend(x = 1.1, y = 1.3,
        legend = rownames(radar_plot_data)[3:6],
        bty = "n", pch = 20, col = colors_border,
